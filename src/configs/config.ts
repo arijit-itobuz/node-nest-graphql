34016b3dc06589dd2b7a9ad2f9906f7f644fa46e
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-09-10 16:34:55 +0530

    feat: init

---
+++ src/configs/config.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+
+export const config = {
+  app: {
+    env: process.env.APP_ENV ?? '',
+    port: process.env.PORT ?? '',
+  },
+  db: {
+    uri: process.env.POSTGRES_URI ?? '',
+  },
+  jwt: {
+    auth_secret: process.env.JWT_AUTH_SECRET ?? '',
+    forgot_password_secret: process.env.JWT_FORGOT_PASSWORD_SECRET ?? '',
+  },
+  smtp: {
+    host: process.env.SMTP_HOST ?? '',
+    port: Number(process.env.SMTP_PORT) ?? 0,
+    username: process.env.SMTP_USERNAME ?? '',
+    password: process.env.SMTP_PASSWORD ?? '',
+  },
+
+  email: {
+    from: process.env.EMAIL_FROM ?? '',
+  },
+};
