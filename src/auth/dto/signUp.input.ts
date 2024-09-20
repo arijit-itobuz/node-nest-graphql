00cddc84c0cafd4d10e4b6ae11851ac3eeb394c3
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-09-20 17:13:31 +0530

    feat: auth

--- src/auth/dto/signUp.input.ts
+++ src/auth/dto/signUp.input.ts
@@ -1,5 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class SignUpInput {
@@ -28,6 +28,7 @@ export class SignUpInput {
 
   @IsNotEmpty()
   @IsString()
+  @IsPhoneNumber()
   @Field(() => String)
   phoneNumber: string;
 
