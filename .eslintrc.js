fb82af1c9d6cfad3a4b565f9d35839c3af8deef1
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-09-27 15:55:10 +0530

    feat: updated eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '/*.js', '/*.mjs'],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
