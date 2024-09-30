e8959111abdae20548070d999594b9727d4b458a
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-09-30 15:11:29 +0530

    feat: added hekmet, cors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { config } from './config/config';
@@ -7,7 +8,25 @@ import { config } from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    methods: ['HEAD,GET,POST,PUT,PATCH,DELETE'],
+    origin: [],
+  });
 
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [`'self'`, 'data:', 'apollo-server-landing-page.cdn.apollographql.com'],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [`'self'`, 'apollo-server-landing-page.cdn.apollographql.com'],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    })
+  );
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalFilters(new GlobalExceptionFilter());
 
